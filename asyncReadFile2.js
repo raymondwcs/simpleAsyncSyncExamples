a7fd411a79db88e021ad5a52a0631bcc7c045019
Raymond SO <rso@ouhk.edu.hk>
2015-09-23 10:17:24 +0800

    added more comments

--- asyncReadFile2.js
+++ asyncReadFile2.js
@@ -1,21 +1,21 @@
 var fs = require('fs');
 
 fs.open('README.md','r',
-        function(err,handle) {
+        function(err,handle) { /* callback for fs.open() */
 			if (err) {
 				console.log("ERROR: " + err.code + " (" + err.message + ")");
 				return;
 			}
 			var buf = new Buffer(100000);
 			fs.read(handle,buf,0,100000,null,
-			        function(err, length) {
+			        function(err, length) {  /* callback for fs.read() */
 						if (err) {
 							console.log("ERROR: " + err.code + " (" + err.message + ")");
 							return;
 						}
 						console.log(buf.toString('utf8',0,length));
 						fs.close(handle,function() {});
-					}
-			); /* close fs.read() */
-		}
-);
+					} /* end of callback for fs.read() */
+			); /* end of fs.read() */
+		} /* end of callback for fs.open() */
+); /* end of fs.open() */
